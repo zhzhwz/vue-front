2f0f3ec49169095192841d347b71373ca8ae7aac
zhzhwz <954753124@qq.com>
2022-02-04 14:11:12 +0800

    modify url

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
     }
   },
   mounted() {
-    axios.get('http://localhost:3000/api/users').then(response => {
+    axios.get('http://zhzhwz.cn:3000/api/users').then(response => {
       this.users = response.data;
     });
   },
