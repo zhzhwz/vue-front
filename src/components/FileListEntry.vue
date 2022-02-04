d935f6d5942510eae2d2aee3fd982680c624f91c
zhzhwz <954753124@qq.com>
2022-02-04 19:21:10 +0800

    add download

--- src/components/FileListEntry.vue
+++ src/components/FileListEntry.vue
@@ -6,15 +6,31 @@
 
 <script>
 export default {
-  name: 'FileListEntry',
-  props: {
-    filename: String,
-  },
-  methods: {
-      download() {
-          console.log(this.filename);
-      },
-  }
+    name: 'FileListEntry',
+    props: {
+        filename: String,
+    },
+    methods: {
+        download() {
+            import axios from 'axios';
+            const url = 'http://zhzhwz.cn/api/fileDownload';
+            axios.post(url, {filename: this.filename}, {responseType: 'blob'})
+            .then((res) => {
+                const { data, headers } = res
+                const fileName = headers['content-disposition'].replace(/\w+;filename=(.*)/, '$1')
+                const blob = new Blob([data], {type: headers['content-type']})
+                let dom = document.createElement('a')
+                let url = window.URL.createObjectURL(blob)
+                dom.href = url
+                dom.download = decodeURI(fileName)
+                dom.style.display = 'none'
+                document.body.appendChild(dom)
+                dom.click()
+                dom.parentNode.removeChild(dom)
+                window.URL.revokeObjectURL(url)
+            }).catch((err) => {console.log('download error: ' + err);})
+        },
+    }
 }
 </script>
 
