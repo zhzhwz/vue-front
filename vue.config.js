588b54cc8f3d73b51889040c21dd8f82f20df20b
zhzhwz <954753124@qq.com>
2022-02-13 19:22:00 +0800

    rename title

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,13 @@ module.exports = {
               }
             }
         }
+    },
+    chainWebpack: config => {
+        config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = 'ZhShare'
+            return args
+        })
     }
 }
