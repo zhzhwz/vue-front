24abc23e53706afc8bef3f91b8ceff5b4b45c291
zhzhwz <954753124@qq.com>
2022-02-13 15:17:27 +0800

    add local debug

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     devServer: {
         disableHostCheck: true,
+        proxy: {
+            '/api': {
+              target: 'http://localhost:3000',
+              changeOrigin: true,
+              ws: true,
+              pathRewrite: {
+                  '^/api': '/api'
+              }
+            }
+        }
     }
 }
